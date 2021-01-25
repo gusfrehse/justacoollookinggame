e1025221593a7200549125115c5f70e0a97a4cb0
Gustavo S Frehse <gusfrehse@gmail.com>
2021-01-25 17:10:31 -0300

    Setting things up for compiling in windows

--- compile.bat
+++ compile.bat
@@ -1,12 +1,16 @@
 :: @echo off
 
 set cc=g++
-set std=-std=c++17
+set std=-std=c++2a
 set idir=-I.\include\
 set ldir=-L.\winlib\
 set libs=-lglfw3 -lgdi32
 set exe=game.exe
 
-call .\sourcefiles.txt
+:: compatibility with linux
+set /p sfilesstring=<srcfiles.txt
+set %sfilesstring%
+set sourcefiles=%sourcefiles:"=%
+echo %sourcefiles%
 
 %cc% -g %std% -o %exe% %sourcefiles% %idir% %ldir% %libs%
